d2bee809d0acb687212c785528308a64dd7f81cd
Archibald-prog <archpod@gmail.com>
2021-12-06 13:02:26 +0300

    Выполнил ДЗ к Уроку 8 курса Django Part 2

--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -1,4 +1,6 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db import connection
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, reverse, get_object_or_404
 from django.urls import reverse_lazy
@@ -7,8 +9,11 @@ from adminapp.forms import ShopUserAdminEditForm, ProductEditForm
 from authapp.forms import ShopUserRegisterForm
 from authapp.models import ShopUser
 from django.contrib.auth.decorators import user_passes_test
+
 from mainapp.models import ProductCategory, Product
 from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # контроллер в формате CBV
 class UsersListView(LoginRequiredMixin, ListView):
@@ -227,3 +232,22 @@ def product_delete(request, pk):
     context = {'title': title, 'product_to_delete': product}
 
     return render(request, 'adminapp/product_delete.html', context)
+
+
+def db_profile_by_type(prefix, type, queries):
+    update_queries = list(filter(lambda x: type in x['sql'], queries))
+    print(f'db_profile {type} for {prefix}: ')
+    [print(query['sql']) for query in update_queries]
+
+
+@receiver(pre_save, sender=ProductCategory)
+def product_is_active_update_productcategory_save(sender, instance, **kwargs):
+    if instance.pk:
+        instance.product_set.update(is_active=instance.is_active)  # упростили код
+
+        # if instance.is_active:
+        #     instance.product_set.update(is_active=True)
+        # else:
+        #     instance.product_set.update(is_active=False)
+
+        db_profile_by_type(sender, 'UPDATE', connection.queries)
--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -6,6 +6,7 @@ from basketapp.models import Basket
 from mainapp.models import Product
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.db.models import F, Q
 
 
 @login_required
@@ -32,6 +33,22 @@ def basket_add(request, pk):
     basket.save()
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+# @login_required
+# def basket_add(request, pk):
+#     if 'login' in request.META.get('HTTP_REFERER'):
+#         return HttpResponseRedirect(reverse('products:detail', args=[pk]))
+#     product = get_object_or_404(Product, pk=pk)
+#
+#     basket = Basket.objects.filter(user=request.user, product=product).first()
+#
+#     if not basket:
+#         basket = Basket(user=request.user, product=product)
+#
+#     basket.quantity += 1
+#     basket.save()
+#
+#     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 @login_required
